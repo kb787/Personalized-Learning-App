46d6a5536876488c82f142f49644bdeef5406943
Karan <karanbhanu788@gmail.com>
2024-02-18 17:40:03 +0530

    minor-changes-in-port-no-calling

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -6,8 +6,8 @@ import coursesRouter from "../features/courses/routers/courses-router";
 import dotenv from 'dotenv'
 const router = Router() ;
 export default () : Router => {
-    router.use('/auth',authRouter) ;
-    router.use('/profile',profileRouter) ;
-    router.use('/course',coursesRouter)
+    router.use('/auth',authRouter()) ;
+    router.use('/profile',profileRouter()) ;
+    router.use('/course',coursesRouter()) ;
     return router ;
 }
